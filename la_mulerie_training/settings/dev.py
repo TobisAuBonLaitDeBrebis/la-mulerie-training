5e6018095e2e79d0ef83c87e067321485057ea21
JosephMounier <joseph.mounier4@gmail.com>
2025-10-04 18:02:52 +0200

    test

--- la_mulerie_training/settings/dev.py
+++ la_mulerie_training/settings/dev.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
@@ -137,5 +137,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / "theme/static",
+]
 
