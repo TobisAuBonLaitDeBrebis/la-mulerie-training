f72101b009086c88fb8c2ee1eab2027249dd79f3
JosephMounier <joseph.mounier4@gmail.com>
2025-10-04 17:14:15 +0200

    fix prod

--- la_mulerie_training/settings/dev.py
+++ la_mulerie_training/settings/dev.py
@@ -136,3 +136,9 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "theme/static",
+]
+
--- la_mulerie_training/settings/prod.py
+++ la_mulerie_training/settings/prod.py
@@ -132,8 +132,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "theme/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
