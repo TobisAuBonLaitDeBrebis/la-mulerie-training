0f0c7a2edcd5422477de733dfcd6d78381adb245
JosephMounier <joseph.mounier4@gmail.com>
2025-10-05 15:03:52 +0200

    switch settings for prod

--- manage.py
+++ manage.py
@@ -6,7 +6,12 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'la_mulerie_training.settings.prod')
+    env = os.environ.get('DJANGO_ENV', 'dev')  # 'dev' par défaut
+    if env.lower() == 'prod':
+        settings_module = 'la_mulerie_training.settings.prod'
+    else:
+        settings_module = 'la_mulerie_training.settings.dev'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
